5cb40bd44b2c5267be489489e1ccc00a13de318f
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-08-24 16:46:47 +0300

    feat: detect geometry type if not specified

--- src/utils/selectTilesets.js
+++ src/utils/selectTilesets.js
@@ -1,4 +1,14 @@
-function selectTilesets(client) {
+/* eslint-disable camelcase */
+
+function detectGeometryType(client, { table, geometry_field }) {
+  const query = `select distinct geometrytype(${geometry_field}) as type from ${table}`;
+
+  return client
+    .query(query)
+    .then(({ rows }) => rows[0] && { table, type: rows[0].type });
+}
+
+async function selectTilesets(client) {
   const query = `
     select
     json_object_agg(
@@ -12,7 +22,21 @@ function selectTilesets(client) {
     ) as tilesets
     from geometry_columns`;
 
-  return client.query(query).then(({ rows }) => rows[0] && rows[0].tilesets);
+  const tilesets = await client
+    .query(query)
+    .then(({ rows }) => rows[0].tilesets);
+
+  const queries = Object.values(tilesets)
+    .filter(tileset => tileset.type === 'GEOMETRY')
+    .map(tileset => detectGeometryType(client, tileset));
+
+  const geometryTypes = await Promise.all(queries);
+
+  geometryTypes.filter(geometryType => !!geometryType).forEach((geometryType) => {
+    tilesets[geometryType.table].type = geometryType.type;
+  });
+
+  return tilesets;
 }
 
 module.exports = selectTilesets;
